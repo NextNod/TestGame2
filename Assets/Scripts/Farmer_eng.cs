9496d1b636c93517b53fe992928adf1fc39e68c5
Алексей Яценко <lexa1973f@gmail.com>
2021-08-06 05:10:46 -0700

    All project

---
+++ Assets/Scripts/Farmer_eng.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class Farmer_eng : MonoBehaviour
+{
+    enum Action { Up, Down, Right, Left, Wait }
+    
+    float lenght = 0;
+    Action nowAction;
+    new SpriteRenderer renderer;
+
+    public Sprite Up;
+    public Sprite Down;
+    public Sprite Right;
+    public Sprite Left;
+    
+    public Sprite Up_dirty;
+    public Sprite Down_dirty;
+    public Sprite Right_dirty;
+    public Sprite Left_dirty;
+    
+    public GameObject Camera;
+    public GameObject Label;
+    public Collider2D bomb;
+    public static bool isGameOver = false;
+
+    void Start() 
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        if (isGameOver) GameOver();
+    }
+
+    [System.Obsolete]
+    void Update()
+    {
+        if (!isGameOver)
+        {
+            if (lenght <= 0)
+            {
+                nowAction = (Action)Random.RandomRange(0, 5);
+                lenght = Random.RandomRange(1f, 5f);
+
+                switch (nowAction)
+                {
+                    case Action.Up:
+                        renderer.sprite = Up;
+                        break;
+
+                    case Action.Down:
+                        renderer.sprite = Down;
+                        break;
+
+                    case Action.Right:
+                        renderer.sprite = Right;
+                        break;
+
+                    case Action.Left:
+                        renderer.sprite = Left;
+                        break;
+                }
+            }
+
+            switch (nowAction)
+            {
+                case Action.Up:
+                    transform.position = Vector2.MoveTowards(transform.position, transform.position + transform.up, Time.deltaTime);
+                    break;
+
+                case Action.Down:
+                    transform.position = Vector2.MoveTowards(transform.position, transform.position + transform.up * -1, Time.deltaTime);
+                    break;
+
+                case Action.Right:
+                    transform.position = Vector2.MoveTowards(transform.position, transform.position + transform.right, Time.deltaTime);
+                    break;
+
+                case Action.Left:
+                    transform.position = Vector2.MoveTowards(transform.position, transform.position + transform.right * -1, Time.deltaTime);
+                    break;
+            }
+
+            lenght -= Time.deltaTime;
+        }
+}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Im in!");
+        var tag = other.gameObject.tag;
+
+        if (tag == "bomb")
+            GameOver();
+        else if (tag == "Player") {
+            isGameOver = true;
+            Label.SetActive(true);
+        }
+    }
+
+    public void GameOver() 
+    {
+        isGameOver = true;
+        switch (nowAction)
+        {
+            case Action.Up:
+                renderer.sprite = Up_dirty;
+                break;
+
+            case Action.Down:
+                renderer.sprite = Down_dirty;
+                break;
+
+            case Action.Right:
+                renderer.sprite = Right_dirty;
+                break;
+
+            case Action.Left:
+                renderer.sprite = Left_dirty;
+                break;
+        }
+        var pos = transform.position;
+        pos.z = -10f;
+        Camera.transform.DOMove(pos, 2f);
+    }
+}
---
+++ Assets/Scripts/Movment_eng.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movment_eng : MonoBehaviour
+{
+    enum Way { Up, Down, Right, Left };
+
+    int width = Screen.width,
+        height = Screen.height;
+
+    public Sprite right;
+    public Sprite left;
+    public Sprite up;
+    public Sprite down;
+
+    public GameObject bomb;
+    public Transform SpavnBomb;
+
+    SpriteRenderer sprite;
+    Way personWay;
+    bool lastToach = false;
+
+    void Start() 
+    {
+        personWay = Way.Right;
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && !Farmer_eng.isGameOver) {
+            var touch = Input.GetTouch(Input.touchCount - 1);
+
+            if (touch.position.x > width - (width / 3)) {
+                if (personWay != Way.Right) {
+                    personWay = Way.Right;
+                    sprite.sprite = right;
+                    SpavnBomb.localPosition = new Vector3(6f, 0f);
+                }
+                transform.position = Vector2.MoveTowards(transform.position, transform.position + transform.right, 2f * Time.deltaTime);
+            }
+            else if (touch.position.x < width / 3) {
+                if (personWay != Way.Left) {
+                    personWay = Way.Left;
+                    sprite.sprite = left;
+                    SpavnBomb.localPosition = new Vector3(-6f, 0f);
+                }
+                transform.position = Vector2.MoveTowards(transform.position, transform.position + transform.right * -1, 2f * Time.deltaTime);
+            }
+            else if (touch.position.y > height - (height / 3)) {
+                if (personWay != Way.Up) {
+                    personWay = Way.Up;
+                    sprite.sprite = up;
+                    SpavnBomb.localPosition = new Vector3(0f, 6f);
+                }
+                transform.position = Vector2.MoveTowards(transform.position, transform.position + transform.up, 2f * Time.deltaTime);
+            }
+            else if (touch.position.y < height / 3) {
+                if (personWay != Way.Down) {
+                    personWay = Way.Down;
+                    sprite.sprite = down;
+                    SpavnBomb.localPosition = new Vector3(0f, -6f);
+                }
+                transform.position = Vector2.MoveTowards(transform.position, transform.position + transform.up * -1, 2f * Time.deltaTime);
+            } else {
+                lastToach = true;
+            }
+        } else if (lastToach) {
+            float x = SpavnBomb.position.x, y = SpavnBomb.position.y;
+            if (y > -4.8f && y < 3.9f && x > -9.9f && x < 9.3f) {
+                lastToach = false;
+                var tmp = Instantiate(bomb);
+                tmp.transform.localPosition = SpavnBomb.position;
+                tmp.transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+    }
+}
